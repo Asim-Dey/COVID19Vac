13afc87cc9753d7946cae278de92decc0d6098e0
Asim-Dey <100403847+Asim-Dey@users.noreply.github.com>
2023-09-23 07:59:14 -0600

    Add files via upload

---
+++ vac_Network_USA_65_V2_Weight.R
@@ -0,0 +1,173 @@
+#install.packages("igraph")
+library(igraph) 
+
+library(ggplot2)
+
+
+D0<-read.csv('C:/Users/adey/OneDrive/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/County_VaccinationsAA.csv')
+
+#D0<-read.csv('C:/Users/asimi/OneDrive/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/County_VaccinationsAA.csv')
+
+names(D0)
+head(D0) 
+
+length(unique(D0$STATE))
+
+
+
+
+boxplot1 = ggplot(D0, aes(x = state, y = Series_Complete_65PlusPop_Pct)) + geom_boxplot()+ 
+  #coord_cartesian(ylim = c(0,30))+
+  coord_flip()
+
+
+boxplot1
+
+#==========================
+boxplot1 = ggplot(D0, aes(x = state, y = covid_cases_per_100k)) + geom_boxplot()+ 
+  #coord_cartesian(ylim = c(0,30))+
+  coord_flip()
+
+
+boxplot1
+
+
+
+
+###################### Distance matrix #############################
+
+#================= Vec Distance matrix ===================
+
+#Series_Complete_65PlusPop_Pct
+
+vec65<-D0$Series_Complete_65PlusPop_Pct
+summary(vec65)
+
+#Min. 1st Qu.  Median    Mean  3rd Qu.    Max. 
+#17.60   73.70   83.10   81.06   91.00   95.00 
+
+#vec18[vec18>70]
+#length(vec18)
+
+DD1<-as.matrix(vec65)
+n<-dim(DD1)[1]
+n
+
+################################################################################
+m<-dim(DD1)[1];m
+M11<- matrix(data=NA,ncol=m,nrow=m)
+
+#DD1[,1]
+#DD1[,2] 
+
+
+
+for(i in 1:m){     # i=1
+  for(j in 1:m){   # j=2
+    
+    
+    M11[i,j] = abs(DD1[i,]- DD1[j,])
+    dim(M11)
+    
+    
+    # print(i)
+    # print(j)
+    
+  }
+}
+
+
+
+S1<-summary(as.numeric(M11))
+S1
+
+
+##===============  Geo Distance matrix =======================
+
+library(geosphere)
+
+#fun = distGeo/distHaversine
+
+M_Goe<-distm(D0[,c(7,8)],fun = distGeo)/ 1609.344 # Distance in miles
+dim(M_Goe)
+
+summary(as.numeric(M_Goe))
+#Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#0.0   476.0   766.2   851.7  1128.3  2858.1 
+#########################################################################
+
+k2=62.46   # Mean or above of 18 netowrk for comparison
+
+#k1=0.05 # Counties which vec rate similar dist less than k2 (less than median)
+DM=50  # Counties which are within "DM" miles are connected by an edge
+
+
+A1 <- matrix(0, nrow = n, ncol =n);dim(A1)
+A2 <- matrix(0, nrow = n, ncol =n);dim(A2)
+A3 <- matrix(0, nrow = n, ncol =n);dim(A3)
+
+############### Step 1 #####################################
+
+for (i in 1:n){ #i=1
+  for (j in 1:n){ #j=2
+    
+    
+    if  (DD1[,1][i] > k2 & DD1[,1][j]> k2)  A2[i,j]=(1/M11[i,j])
+    if  (DD1[,1][i] > k2 & DD1[,1][j]> k2 & M_Goe[i,j] < DM)  A1[i,j]= (1/M11[i,j])
+    if  (DD1[,1][i] > k2 & DD1[,1][j]> k2 & M_Goe[i,j] < DM)  A3[i,j]=1
+    
+    
+  }
+  
+  print(i)
+}
+
+
+
+dim(A1)
+dim(A2)
+dim(A3)
+
+
+
+#write.csv(A3,'C:/Users/adey/OneDrive/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/edlist_65_Dist_V2_aboveMean_WeightA3.csv')
+
+
+
+###################### Network ###################
+
+
+
+G0<- graph.adjacency(A1, mode="undirected",weighted=TRUE,diag=FALSE)
+
+V0<-length(V(G0));V0 # 3193
+E0<-length(E(G0));E0 #  16743
+
+plot(G0, vertex.size=5, 
+     vertex.label=NA,vertex.color="red",edge.arrow.size=0.9)
+
+
+
+#G1<- graph.adjacency(A2, mode="undirected",weighted=TRUE,diag=FALSE)
+
+
+#V<-length(V(G1));V # 3193
+#E<-length(E(G1));E # 4326211
+
+
+#plot(G1, vertex.size=5, 
+#    vertex.label=NA,vertex.color="red",edge.arrow.size=0.9)
+
+
+
+
+edlist_65_Dist<-cbind(get.edgelist(G0))
+
+
+#edlist_65<-cbind(get.edgelist(G1))
+
+#write.csv(edlist_65,'C:/Users/asimi/OneDrive/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/edlist_65_V2.csv')
+
+#write.csv(edlist_65_Dist,'C:/Users/adey/OneDrive/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/edlist_65_Dist_V2_aboveMean.csv')
+
+
