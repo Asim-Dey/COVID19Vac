b588de703c4323e046fff1271ec74a4e0b83740b
Asim-Dey <100403847+Asim-Dey@users.noreply.github.com>
2023-09-23 07:55:27 -0600

    Update ERGM_Model_65.R

--- ERGM_Model_65.R
+++ ERGM_Model_65.R
@@ -3,10 +3,8 @@
 library(igraph) 
 library(ggplot2)
 
+D0<-read.csv(County_VaccinationsAA.csv')
 
-D0<-read.csv('Library/CloudStorage/OneDrive-Personal/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/County_VaccinationsAA.csv')
-#D0<-read.csv('C:/Users/adey/OneDrive/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/County_VaccinationsAA.csv')
-#D0<-read.csv('C:/Users/asimi/OneDrive/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/County_VaccinationsAA.csv')
 
 names(D0)
 head(D0) 
@@ -17,7 +15,7 @@ length(unique(D0$STATE))
 
 ################ 18 #####################################
 
-A0<-read.csv("Library/CloudStorage/OneDrive-Personal/Mixed Effect Model/Data and Code/Data/COVID-19 Vaccinations in the US/edlist_65_Dist_V2_aboveMean.csv")
+A0<-read.csv("edlist_65_Dist_V2_aboveMean.csv")
 
 
 G01<-graph_from_edgelist(as.matrix(A0))
